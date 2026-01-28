5a87a50611e75c3e65a559aee525832d7f328d3b
ParkSW <rty3394@naver.com>
2026-01-28 17:12:58 +0900

    Movement, GAS, GameplayWidget

---
+++ Source/Crunch/Private/Character/CCharacter.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "CCharacter.generated.h"
+
+UCLASS()
+class ACCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACCharacter();
+	void ServerSideInit();
+	void ClientSideInit();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/****************************************************************************************/
+	/*									GameplayAbility										*/
+	/****************************************************************************************/
+public:
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Gameplay Ability")
+	class UCAbilitySystemComponent* CAbilitySystemComponent;
+	UPROPERTY()
+	class UCAttributeSet* CAttributeSet;
+
+};
---
+++ Source/Crunch/Private/Player/CPlayerController.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	// only called on the server
+	void OnPossess(APawn* NewPawn) override;
+	// only called on the client, also on the listening server
+	void AcknowledgePossession(APawn* NewPawn) override;
+private:
+	void SpawnGameplayWidget();
+
+	UPROPERTY()
+	class ACPlayerCharacter* CPlayerCharacter;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<class UGameplayWidget> GameplayWidgetClass;
+
+	UPROPERTY()
+	class UGameplayWidget* GameplayWidget;
+};
